5a97c07dd60de937171de742e0edeb0bea438e45
JovanAdamovic <jadamovic538@gmail.com>
2026-03-10 01:44:58 +0100

    26. Priprema za produkciju

--- evidencije_react/src/api/http.js
+++ evidencije_react/src/api/http.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const baseURL =
+  process.env.REACT_APP_API_BASE_URL ||
+  process.env.REACT_APP_API_URL ||
+  "http://127.0.0.1:8080/api";
+
 const http = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL,
   headers: {
     Accept: "application/json",
   },
